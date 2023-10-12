b2aea59e05c65be8f7259528ccd8b530e8a14670
Md. Nayeem Islam <146013558+md-nayeem-islam@users.noreply.github.com>
2023-10-12 19:21:28 +0600

    Add files via upload

---
+++ tailwind-css/README.md
@@ -0,0 +1,2 @@
+# tailwind-css
+live preview https://md-nayeem-islam.github.io/tailwind-css/
---
+++ tailwind-css/js/telwind.config.js
@@ -0,0 +1,34 @@
+
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            clifford: '#da373d',
+            black: '#0D0D0D',
+            btn: '#FFD2A4',
+            lineargradient : '#000,#000',
+          },
+          maxWidth: {
+            'containar': '1200px',
+          },
+          backgroundImage: {
+            'banner': "url('images/banners.jpg')",
+            mosque : "url('images/mosque (1).jpg')",
+          },
+          fontSize: {
+            heaing: '54px',
+          },
+          lineHeight: {
+            'line-height': '120%',
+          },
+          fontFamily: {
+            'Roboto': "'Roboto', sans-serif;"
+          },
+          extend: {
+            backdropOpacity: {
+              filter: ' blur(20px);',
+            }
+        }
+      }
+    }
+  }
